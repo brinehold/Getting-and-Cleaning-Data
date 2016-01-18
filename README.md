95999fbcdc97b6e83968ae0b9927b97f5539d89d
Brett Rinehold <brinehold@gmail.com>
2016-01-18 15:48:18 -0800

    Course Project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Getting-and-Cleaning-Data
-Getting and Cleaning Data
+This file describes how to run the run_analysis.R script and create a tidy data set
+* First, unzip the data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and save to your working directory.
+* Second, use source("run_Analysis.R") command in RStudio. 
+* Running the script will generate two data files:
+  - tidyData.txt : contains the tidy dataset in accordance with the project objectives.
+  - MeanData.txt : contains the average of each activity for each subject
+* You could use read.table to open the file in RStudio if you wanted. 
---
+++ run_Analysis.R
@@ -0,0 +1,64 @@
+setwd("~/Documents/Coursera/Getting and Cleaning Data")
+# Step1. Merges the training and the test sets to create one data set.
+# Load Train Data
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainNames <- read.table("./UCI HAR Dataset/train/y_train.txt")
+trainSub <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+# Load Test Data
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testNames <- read.table("./UCI HAR Dataset/test/y_test.txt") 
+testSub <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+# Merge Train and Test Data
+mergeData <- rbind(trainData, testData)
+mergeNames <- rbind(trainNames, testNames)
+mergeSub <- rbind(trainSub, testSub)
+
+# Step2. Extracts only the measurements on the mean and standard deviation 
+# for each measurement. 
+features <- read.table("./UCI HAR Dataset/features.txt")
+meanIdx <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+mergeData <- mergeData[, meanIdx]
+
+# Clean names and Filter Names
+names(mergeData) <- gsub("\\(\\)", "", features[meanIdx, 2]) # remove "()"
+names(mergeData) <- gsub("mean", "Mean", names(mergeData)) # capitalize Mean
+names(mergeData) <- gsub("std", "Std", names(mergeData)) # capitalize Std
+names(mergeData) <- gsub("-", "", names(mergeData)) # remove "-" in col names 
+
+# Step3. Uses descriptive activity names to name the activities in 
+# the data set
+activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
+activityNames <- activity[mergeNames[, 1], 2]
+mergeNames[, 1] <- activityNames
+
+# Step4. Appropriately labels the data set with descriptive activity 
+# names. 
+tidyData <- cbind(mergeSub, mergeNames, mergeData)
+colnames(tidyData)[1:2] <- c("Subject", "Activity")
+write.table(tidyData, "tidy_data.txt") # write out the 1st dataset
+
+# Step5. Creates a second, independent tidy data set with the average of 
+# each variable for each activity and each subject. 
+subjectLen <- length(table(mergeSub))
+activityLen <- dim(activity)[1]
+columnLen <- dim(tidyData)[2]
+result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen) 
+result <- as.data.frame(result)
+colnames(result) <- colnames(tidyData)
+row <- 1
+for(ii in 1:subjectLen) {
+        for(jj in 1:activityLen) {
+                result[row, 1] <- sort(unique(mergeSub)[, 1])[ii]
+                result[row, 2] <- activity[jj, 2]
+                idx1 <- ii == tidyData$subject
+                idx2 <- activity[jj, 2] == tidyData$activity
+                result[row, 3:columnLen] <- colMeans(tidyData[idx1&idx2, 3:columnLen])
+                row <- row + 1
+        }
+}
+write.table(result, "MeanData.txt") # write out the mean data.
